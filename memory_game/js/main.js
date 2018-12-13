4a8c193cbc160279c31a69a48523118828c74fd4
Valerie Komova <valeriyakomova@gmail.com>
2018-12-12 21:41:04 -1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,20 @@
-console.log("Up and running!");
+var cards = ["queen", "queen", "king", "king"]
+var cardsInPlay = []
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cardOne = cards[0]
+cardsInPlay.push('cardOne');
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cardTwo = cards[2]
+cardsInPlay.push('cardTwo');
+
+console.log("User flipped queen");
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+	
+} }
