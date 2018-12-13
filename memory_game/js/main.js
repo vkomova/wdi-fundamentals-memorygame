2dc12e02f1d6cfdcb883981d3d7106a59a17cb49
Valerie Komova <valeriyakomova@gmail.com>
2018-12-12 21:46:25 -1000

    updated some more code for game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -10,11 +10,8 @@ cardsInPlay.push('cardTwo');
 console.log("User flipped queen");
 console.log("User flipped king");
 
-if (cardsInPlay.length === 2) {
-
-if (cardsInPlay[0] === cardsInPlay[1]) {
+if ((cardsInPlay.length === 2) && (cardsInPlay[0] === cardsInPlay[1])) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
-	
-} }
+} 
